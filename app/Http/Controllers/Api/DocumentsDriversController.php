639436cf6da87edcb38e271827acc921e6ee32ab
gesoft <--global>
2021-12-28 19:04:28 -0500

    Se agrega modelo de documentos de conductores

--- app/Http/Controllers/Api/DocumentsDriversController.php
+++ app/Http/Controllers/Api/DocumentsDriversController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Storedocuments_driversRequest;
 use App\Http\Requests\Updatedocuments_driversRequest;
 use App\Models\documents_drivers;
@@ -11,11 +12,32 @@ class DocumentsDriversController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        //
+        $code_response = 200; // OK
+        $message = "Datos encontrados";
+        $error = true; // Error response
+        $data = [];
+
+        try {
+            $documents = documents_drivers::all();
+            if($documents->count()){
+                $error = false;
+            } else {
+                $message = "No existen documentos";
+            }
+        } catch (\Exception $e){
+            $code_response = 500;
+            $message = $e->getMessage();
+        }
+
+        return response()->json([
+            'message' => $message,
+            'error' => $error,
+            'data' => $data,
+        ], $code_response);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/all-documents', [\App\Http\Controllers\Api\DocumentsDriversController::class, 'index']);
