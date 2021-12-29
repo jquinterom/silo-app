d7d8e3a51f060adec262db1bedbfa53dd4d05915
gesoft <--global>
2021-12-28 23:45:34 -0500

    Se agrega boostrap y jquery

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,13 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+
     </head>
     <body class="antialiased">
         <div class="container">
@@ -29,10 +36,15 @@
                         @csrf
                         <input type="file" name="urlpdf" >
                         <input type="submit" value="subir">
+                        <input type="button" value="subir" id="btnSend" class="btn btn-primary">
                     </form>
 
                 </div>
             </div>
         </div>
+
+        <script src="{{ asset('/js/app.js') }}"></script>
+
     </body>
+
 </html>
